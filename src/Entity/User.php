d402e141d8b8a1758861c3827bc85de65721a678
Stébou Mendy <stebou8mendy@gmail.com>
2022-09-11 10:58:37 +0200

    Install api and validation for user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,21 +2,34 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ApiResource(
+    normalizationContext: ['groups' => ['users_read']],
+    denormalizationContext: ['disable_type_enforcement' => true]
+)]
+#[UniqueEntity('email', message: 'Un utilisateur possède déjà cet email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(["users_read"])]
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
-    private ?string $email = null;
+    #[Groups(["users_read"])]
+    #[Assert\NotBlank(message: "L'email doit être renseigné")]
+    #[Assert\Email(message: "L'email doit avoir un format valide")]
+    private $email = null;
 
     #[ORM\Column]
     private array $roles = [];
@@ -25,15 +38,20 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @var string The hashed password
      */
     #[ORM\Column]
+    #[Assert\NotBlank(message: "Le mot de passe doit être renseigné")]
+    #[Assert\Length(min:3, max: 255, minMessage: "Le mot de passe doit faire entre 3 et 255 caractères", maxMessage: "Le mot de passe doit faire entre 3 et 255 caractères")]
     private ?string $password = null;
 
     #[ORM\Column(nullable: true)]
+    #[Groups(["users_read"])]
     private ?float $latitude = null;
 
     #[ORM\Column(nullable: true)]
+    #[Groups(["users_read"])]
     private ?float $longitude = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(["users_read"])]
     private ?string $deviceId = null;
 
     public function getId(): ?int
@@ -46,7 +64,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this->email;
     }
 
-    public function setEmail(string $email): self
+    public function setEmail($email): self
     {
         $this->email = $email;
 
